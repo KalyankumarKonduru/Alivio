ed35dac021f11fb1b63811ac33eeb0e10fcf161e
Kalyankumar Konduru <kondk01@pfw.edu>
2025-04-14 01:20:09 -0400

    changes to auth

--- frontend/src/utils/setAuthToken.js
+++ frontend/src/utils/setAuthToken.js
@@ -1,5 +1,16 @@
 import axios from 'axios';
 
+const apiBaseUrl = process.env.REACT_APP_API_URL;
+
+// Example API call
+axios.get(`${apiBaseUrl}/api/https://alivio.onrender.com`)
+  .then(response => {
+    console.log('Data:', response.data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
 const setAuthToken = token => {
   if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
